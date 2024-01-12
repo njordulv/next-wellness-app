093d425003f29291279d5f2d10b8c24fb19b795b
njordulv <njordulv@gmail.com>
2024-01-12 20:24:58 +0200

    ADD: checkout page and logic; API email & checkout ref

--- api/index.js
+++ api/index.js
@@ -1,75 +1,18 @@
 const express = require('express')
 const cors = require('cors')
-const fs = require('fs')
 const app = express()
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-function readFileAsync(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(`Error reading file ${filePath}: ${err}`)
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
-  })
-}
+// Import route handlers
+const emailHandler = require('./email')
+const checkoutHandler = require('./checkout')
 
-function writeFileAsync(filePath, content) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, content, 'utf8', (err) => {
-      if (err) {
-        console.error(`Error writing file ${filePath}: ${err}`)
-        reject(err)
-      } else {
-        resolve()
-      }
-    })
-  })
-}
-
-app.post('/submit-email', async (req, res) => {
-  try {
-    const formData = req.body
-    let data = await readFileAsync('data/email.json')
-
-    if (!data) {
-      data = '[]'
-    }
-
-    const emailData = JSON.parse(data)
-    emailData.push(formData)
-
-    await writeFileAsync('data/email.json', JSON.stringify(emailData, null, 2))
-    res.status(200).send('Data was saved successfully')
-  } catch (err) {
-    res.status(500).send('Error saving data')
-  }
-})
-
-app.post('/submit-checkout', async (req, res) => {
-  try {
-    const checkoutData = req.body
-    const data = await readFileAsync('data/checkout.json')
-    const checkoutArray = JSON.parse(data)
-
-    checkoutArray.push(checkoutData)
-
-    await writeFileAsync(
-      'data/checkout.json',
-      JSON.stringify(checkoutArray, null, 2)
-    )
-
-    res.status(200).send('Form submitted successfully!')
-  } catch (err) {
-    res.status(500).send('Error submitting form')
-  }
-})
+// Use the route handlers
+app.use('/submit-email', emailHandler)
+app.use('/submit-checkout', checkoutHandler)
 
 app.listen(4000, () => {
   console.log('Server running on port 4000')
---
+++ src/app/components/checkout/Final.jsx
@@ -0,0 +1,124 @@
+import { useRouter } from 'next/navigation'
+import styled from 'styled-components'
+import { VscChromeClose } from 'react-icons/vsc'
+
+const Popup = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  display: none;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+
+  &.show {
+    display: flex;
+  }
+
+  &.hide {
+    display: none;
+  }
+`
+
+const PopupWrapper = styled.div`
+  position: relative;
+  transition: all 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: var(--black);
+  padding: 50px 20px;
+  border-radius: 10px;
+  width: 100%;
+  background-size: 200% 200%;
+  animation: fadeIn 0.35s ease-in-out;
+  z-index: 9;
+
+  &::after {
+    position: absolute;
+    content: '';
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: -2;
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    transform: scale(0.96) translateZ(0);
+    filter: blur(20px);
+    background: linear-gradient(
+      to left,
+      var(--pink),
+      var(--green),
+      var(--blue),
+      var(--purple)
+    );
+    background-size: 200% 200%;
+    animation: animateGlow 3s linear infinite;
+  }
+
+  &::before {
+    position: absolute;
+    content: '';
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    z-index: -1;
+    background: var(--background-darker);
+  }
+
+  span {
+    color: var(--green);
+  }
+`
+
+const ClosePopup = styled.button`
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  color: var(--pink);
+  font-size: 22px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    color: var(--color);
+  }
+`
+
+const Final = ({ setPopup, emailValue }) => {
+  const router = useRouter()
+  const showPopup = setPopup ? 'show' : 'hide'
+
+  const closePopupHandler = () => {
+    setPopup(false)
+    router.push('/')
+  }
+
+  return (
+    <Popup className={showPopup}>
+      <PopupWrapper>
+        <h2>Your Order is Confirmed</h2>
+        <h3>
+          Thank you for choosing <br />
+          React Fasting App!
+        </h3>
+        <h5>
+          An email to <span>{emailValue}</span> was sent with your personalized
+          code to register on our platform.
+        </h5>
+        <ClosePopup onClick={closePopupHandler}>
+          <VscChromeClose />
+        </ClosePopup>
+      </PopupWrapper>
+    </Popup>
+  )
+}
+
+export default Final
