75165c9530f40d63baeab7b68ac0c583e3649a20
njordulv <njordulv@gmail.com>
2024-01-17 18:00:00 +0200

    UPD: 404 not a quiz page

--- src/app/quiz/[quizSlug]/page.jsx
+++ src/app/quiz/[quizSlug]/page.jsx
@@ -4,6 +4,13 @@ import quizPages from '../../data/quizPages'
 export function generateMetadata({ params }) {
   const quiz = quizPages.find((page) => page.slug === params.quizSlug)
 
+  if (!quiz) {
+    return {
+      title: 'Page Not Found',
+      description: 'Sorry, the requested page was not found',
+    }
+  }
+
   return {
     title: quiz.title,
     description: quiz.description,
