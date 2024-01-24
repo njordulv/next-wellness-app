5278dfc5df7e24e4dce328725a83711015819599
njordulv <njordulv@gmail.com>
2024-01-24 15:57:29 +0200

    UPD: configs for images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ['dummyimage.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dummyimage.com',
+      },
+    ],
   },
 }
+
+module.exports = nextConfig
--- src/app/data/testimonials.ts
+++ src/app/data/testimonials.ts
@@ -8,42 +8,42 @@ interface TestimonialsData {
 export const testimonials: TestimonialsData[] = [
   {
     id: 1,
-    image: 'http://dummyimage.com/100x100.png/dedede/333333',
+    image: 'https://dummyimage.com/100x100.png/dedede/333333',
     name: 'Thomas Clark',
     message:
       'React Fasting App helped me achieve my weight loss goals effectively!',
   },
   {
     id: 2,
-    image: 'http://dummyimage.com/100x100.png/ccaadd/ffffff',
+    image: 'https://dummyimage.com/100x100.png/ccaadd/ffffff',
     name: 'Jane Smith',
     message:
       'I love the simplicity and effectiveness of React Fasting App. It made fasting easy!',
   },
   {
     id: 3,
-    image: 'http://dummyimage.com/100x100.png/5fa2dd/ffffff',
+    image: 'https://dummyimage.com/100x100.png/5fa2dd/ffffff',
     name: 'Michael Johnson',
     message:
       'Using React Fasting App was a game-changer for my health and fitness journey.',
   },
   {
     id: 4,
-    image: 'http://dummyimage.com/100x100.png/dddddd/000000',
+    image: 'https://dummyimage.com/100x100.png/dddddd/000000',
     name: 'Emily Wilkinson',
     message:
       'I highly recommend React Fasting App to anyone looking for a reliable fasting guide!',
   },
   {
     id: 5,
-    image: 'http://dummyimage.com/100x100.png/5fa2ee/ffffff',
+    image: 'https://dummyimage.com/100x100.png/5fa2ee/ffffff',
     name: 'Dave Brown',
     message:
       'The features of React Fasting App are well-thought-out and user-friendly.',
   },
   {
     id: 6,
-    image: 'http://dummyimage.com/100x100.png/dddfff/0a0a0b',
+    image: 'https://dummyimage.com/100x100.png/dddfff/0a0a0b',
     name: 'Sophia Miller',
     message:
       'React Fasting App is my go-to tool for maintaining a healthy lifestyle!',
