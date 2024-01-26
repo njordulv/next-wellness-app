7ea99c97782d4abf1519f8796c69d4c7864a62f5
njordulv <njordulv@gmail.com>
2024-01-26 20:45:12 +0200

    ADD: redirects

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
