31bc551b6df674cfa83ec8605ba783ee2f2efbfe
njordulv <njordulv@gmail.com>
2024-02-02 14:53:24 +0200

    ADD: testimonials FC reorganization + add translations

--- src/app/components/slider/testimonials.tsx
+++ src/app/components/slider/testimonials.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { Component } from 'react'
+import { useTranslations } from 'next-intl'
 import Slider from 'react-slick'
 import Image from 'next/image'
 import { IoStarSharp } from 'react-icons/io5'
@@ -9,73 +9,75 @@ import { testimonials } from '@/data/testimonials'
 import './css/slick.scss'
 import './css/slick-theme.scss'
 
-export default class TestimonialSlider extends Component {
-  render() {
-    const settings = {
-      className: 'center',
-      centerMode: true,
-      infinite: true,
-      centerPadding: '50px',
-      slidesToShow: 1,
-      speed: 700,
-      autoplay: true,
-      pauseOnHover: false,
-      nav: true,
-      dots: true,
-      arrows: false,
-      swipeToSlide: true,
-      responsive: [
-        {
-          breakpoint: 640,
-          settings: {
-            centerPadding: '30px',
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            centerPadding: '20px',
-          },
-        },
-      ],
-    }
+const TestimonialSlider = () => {
+  const t = useTranslations('Testimonials')
 
-    const stars = Array(5).fill(0)
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '50px',
+    slidesToShow: 1,
+    speed: 700,
+    autoplay: true,
+    pauseOnHover: false,
+    nav: true,
+    dots: true,
+    arrows: false,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          centerPadding: '30px',
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerPadding: '20px',
+        },
+      },
+    ],
+  }
 
-    return (
-      <>
-        <Slider {...settings}>
-          {testimonials.map((testimonial) => (
-            <div className="slide-item" key={testimonial.id}>
-              <div className="slide-inner">
-                <div className="relative flex flex-col items-center gap-3 sm:flex-row">
-                  <Image
-                    src={testimonial.image}
-                    width={50}
-                    height={50}
-                    sizes="50x50"
-                    alt="testimonial-image"
-                    priority={false}
-                  />
-                  <span className="relative flex items-center right-0 top-0 gap-1 sm:absolute">
-                    <SiTrustpilot className="text-[17px] text-customGreen" />
-                    Trustpilot
-                  </span>
-                  <div className="flex flex-col items-center gap-2 sm:items-start">
-                    <h4>{testimonial.name}</h4>
-                    <div className="flex gap-1">
-                      {stars.map((_, index) => (
-                        <IoStarSharp key={index} className="text-orange" />
-                      ))}
-                    </div>
+  const stars = Array(5).fill(0)
+
+  return (
+    <>
+      <Slider {...settings}>
+        {testimonials.map((testimonial) => (
+          <div className="slide-item" key={testimonial.id}>
+            <div className="slide-inner">
+              <div className="relative flex flex-col items-center gap-3 sm:flex-row">
+                <Image
+                  src={testimonial.image}
+                  width={50}
+                  height={50}
+                  sizes="50x50"
+                  alt="testimonial-image"
+                  priority={false}
+                />
+                <span className="relative flex items-center right-0 top-0 gap-1 sm:absolute">
+                  <SiTrustpilot className="text-[17px] text-customGreen" />
+                  Trustpilot
+                </span>
+                <div className="flex flex-col items-center gap-2 sm:items-start">
+                  <h4>{t(testimonial.name)}</h4>
+                  <div className="flex gap-1">
+                    {stars.map((_, index) => (
+                      <IoStarSharp key={index} className="text-orange" />
+                    ))}
                   </div>
                 </div>
-                <div className="text-[16px]">{testimonial.message}</div>
               </div>
+              <div className="text-[16px]">{t(testimonial.message)}</div>
             </div>
-          ))}
-        </Slider>
-      </>
-    )
-  }
+          </div>
+        ))}
+      </Slider>
+    </>
+  )
 }
+
+export default TestimonialSlider
--- src/app/data/testimonials.ts
+++ src/app/data/testimonials.ts
@@ -9,43 +9,37 @@ export const testimonials: TestimonialsData[] = [
   {
     id: 1,
     image: 'https://dummyimage.com/100x100.png/dedede/333333',
-    name: 'Thomas Clark',
-    message:
-      'React Fasting App helped me achieve my weight loss goals effectively!',
+    name: 'name1',
+    message: 'message1',
   },
   {
     id: 2,
     image: 'https://dummyimage.com/100x100.png/ccaadd/ffffff',
-    name: 'Jane Smith',
-    message:
-      'I love the simplicity and effectiveness of React Fasting App. It made fasting easy!',
+    name: 'name2',
+    message: 'message2',
   },
   {
     id: 3,
     image: 'https://dummyimage.com/100x100.png/5fa2dd/ffffff',
-    name: 'Michael Johnson',
-    message:
-      'Using React Fasting App was a game-changer for my health and fitness journey.',
+    name: 'name3',
+    message: 'message3',
   },
   {
     id: 4,
     image: 'https://dummyimage.com/100x100.png/dddddd/000000',
-    name: 'Emily Wilkinson',
-    message:
-      'I highly recommend React Fasting App to anyone looking for a reliable fasting guide!',
+    name: 'name4',
+    message: 'message4',
   },
   {
     id: 5,
     image: 'https://dummyimage.com/100x100.png/5fa2ee/ffffff',
-    name: 'Dave Brown',
-    message:
-      'The features of React Fasting App are well-thought-out and user-friendly.',
+    name: 'name5',
+    message: 'message5',
   },
   {
     id: 6,
     image: 'https://dummyimage.com/100x100.png/dddfff/0a0a0b',
-    name: 'Sophia Miller',
-    message:
-      'React Fasting App is my go-to tool for maintaining a healthy lifestyle!',
+    name: 'name6',
+    message: 'message6',
   },
 ]
